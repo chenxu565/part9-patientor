0475bb0d89593b19466a902c52b4bba7f1b4f12b
Chen Xu <cxu@MacBook-Pro.local>
2024-02-28 22:11:26 +0200

    Ex. 9.11 done. Create data type `Patient` and set up the `GET` endpoint `/api/patients`

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import cors from 'cors';
 import express from 'express';
 import diagnosisRouter from './routes/diagnoses';
+import patientRouter from './routes/patients';
 
 const app = express();
 app.use(express.json());
@@ -14,6 +15,7 @@ app.get('/api/ping', (_req, res) => {
 });
 
 app.use('/api/diagnoses', diagnosisRouter);
+app.use('/api/patients', patientRouter);
 
 const PORT = 3001;
 app.listen(PORT, () => {
