ada63acaeb37bb09271fbdc2e8f39c98bbf9a451
Chen Xu <cxu@MacBook-Pro.local>
2024-03-06 07:56:56 +0200

    Commented out express.static('dist') in app.use()

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors());
-app.use(express.static('dist'));
+// app.use(express.static('dist'));
 
 app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
