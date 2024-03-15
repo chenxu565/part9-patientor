fe2c90c9d19fa7c95192dafccbc7edf03aeb17f5
Chen Xu <cxu.self@gmail.com>
2024-03-15 08:45:53 +0200

    Add emptyIcon to HealthRatingBar component

--- patientor-frontend/src/components/HealthRatingBar.tsx
+++ patientor-frontend/src/components/HealthRatingBar.tsx
@@ -1,6 +1,6 @@
 import { Rating } from '@mui/material';
 import { Favorite } from '@mui/icons-material';
-
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import { styled } from '@mui/material/styles';
 import { getColorForRating, HEALTHBAR_TEXTS } from '../utils';
 
@@ -27,6 +27,7 @@ const HealthRatingBar = ({ rating, showText }: BarProps) => {
         value={4 - rating}
         max={4}
         icon={<Favorite fontSize="inherit" sx={{ color }} />}
+        emptyIcon={<FavoriteBorderIcon fontSize="inherit" sx={{ color: "grey" }}/>}
       />
 
       {showText ? <p>{HEALTHBAR_TEXTS[rating]}</p> : null}
